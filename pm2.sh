680fbced20b8a0e55d1d01a8edbff1727abbe69c
root <root@nruf-dev.c.glass-gasket-191818.internal>
2019-03-15 07:33:00 +0000

    Successful setting

--- pm2.sh
+++ pm2.sh
@@ -1,7 +1,7 @@
 now=$(date +'%d/%m/%Y'+'%T'); echo "-------$now------" &&
-killall screen; echo "Terminate old screen" &&
+pm2 delete facade; echo "Terminate old running" &&
 cd /home/artcheeze/nruf-backend && 
 art@bangmod59 | git pull &&
-echo "Reposity-----cloned" &&
-screen -d -m node facade.js &&
+echo "[Reposity cloned]" &&
+pm2 start facade.js &&
 echo "----------Success Deploy-----------" 
