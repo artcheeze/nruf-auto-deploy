6c44a95ec1c58b081f2c81152217becd2006b929
artcheeze <root@nruf-dev.c.glass-gasket-191818.internal>
2019-03-30 11:33:43 +0000

    edit shell

--- r.sh
+++ r.sh
@@ -1,7 +1,10 @@
 now=$(date +'%d/%m/%Y'+'%T'); echo "-------$now------" &&
 killall screen; echo "Terminate old screen" &&
 cd /home/artcheeze/nruf-backend && 
-git pull && npm install &&
+git pull && sudo npm install &&
+cd external-system && sudo npm install &&	
 echo "Cloned and Install Dependencies!" &&
+screen -d -m node index.js &&
+cd .. &&
 screen -d -m node facade.js &&
 echo "----------Success Deploy-----------" 
